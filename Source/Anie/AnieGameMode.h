832bf100b5141b010368c652642ea572d8aa2cdd
sugen.sg <sugen.sg@alibaba-inc.com>
2024-01-19 16:41:05 +0800

    Init

---
+++ Source/Anie/AnieGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AnieGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAnieGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAnieGameMode();
+};
+
+
+
